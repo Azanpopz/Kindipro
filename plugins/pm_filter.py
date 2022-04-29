8e0d7dee69f635864128efb43ed0c0258ade0087
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-04-29 23:19:04 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2605,7 +2605,7 @@ async def auto_filter(client, msg: pyrogram.types.Message, spoll=False):
             logger.exception(e)
 
             fmsg = await message.reply_photo(
-                   caption=f"cap",
+                   caption=f"👮‍♂ {message.from_user.mention} ɴᴏᴛɪᴄᴇ :ɪ𝙵 ʏᴏᴜ ᴅᴏ ɴᴏᴛ sᴇᴇ ᴛʜᴇ 𝙵ɪʟᴇ𝚂 ᴏ𝙵 ᴛʜɪ𝚂 ᴍᴏᴠɪᴇ ʏᴏᴜ ᴀ𝚂ᴋᴇᴅ 𝙵ᴏʀ. ʟᴏᴏᴋ ᴀᴛ ɴᴇ𝚇ᴛ ᴘᴀɢᴇ🔎\n©️քօաɛʀɛɖ ɮʏ :{message.chat.title}",
                    photo="https://telegra.ph/file/8a8ba3e824e1d2482253f.jpg",
                    parse_mode="html",
                    reply_markup=InlineKeyboardMarkup(btn))
@@ -2613,7 +2613,7 @@ async def auto_filter(client, msg: pyrogram.types.Message, spoll=False):
     else:
 
         fmsg = await message.reply_photo(
-               caption=f"cap",
+               caption=f"👮‍♂ {message.from_user.mention} ɴᴏᴛɪᴄᴇ :ɪ𝙵 ʏᴏᴜ ᴅᴏ ɴᴏᴛ sᴇᴇ ᴛʜᴇ 𝙵ɪʟᴇ𝚂 ᴏ𝙵 ᴛʜɪ𝚂 ᴍᴏᴠɪᴇ ʏᴏᴜ ᴀ𝚂ᴋᴇᴅ 𝙵ᴏʀ. ʟᴏᴏᴋ ᴀᴛ ɴᴇ𝚇ᴛ ᴘᴀɢᴇ🔎\n©️քօաɛʀɛɖ ɮʏ :{message.chat.title}",
                photo="https://telegra.ph/file/8a8ba3e824e1d2482253f.jpg",
                parse_mode="html",
                reply_markup=InlineKeyboardMarkup(btn))
