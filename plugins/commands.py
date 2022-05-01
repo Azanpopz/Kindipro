cb9e6ed6404f7a6a30c349a2b120ed1994de0127
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-01 20:41:35 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -274,7 +274,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup(buttons),
+        reply_markup=InlineKeyboardMarkup,
         protect_content=True if pre == 'filep' else False,
         )
                     
