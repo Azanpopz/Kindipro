09ab039a50742a79920cf1719cc5a2a49a1f26f7
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-01 11:47:38 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -96,7 +96,9 @@ async def start(client, message):
         if message.command[1] != "subscribe":
             kk, file_id = message.command[1].split("_", 1)
             pre = 'checksubp' if kk == 'filep' else 'checksub'
-            btn.append([InlineKeyboardButton("❔ T R Y  N O W ❔", callback_data=f"{pre}#{file_id}")])
+            btn.append([InlineKeyboardButton(" 🔄 Try Again", callback_data=f"{pre}#{file_id}")])
+        except IndexError:
+            btn.append([InlineKeyboardButton(" 🔄 Try Again", url=f"https://t.me/{temp.U_NAME}/{message.command[1]}")])
         await client.send_message(
             chat_id=message.from_user.id,
             text="**Please Join Our Channel to Get Movies!**",
