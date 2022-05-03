43a9c4b8e8928597888e0624f7713f7f3a307662
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-03 22:40:07 +0530

    Create telegraph.py

---
+++ plugins/modules/telegraph.py
@@ -0,0 +1,45 @@
+import os
+import shutil
+from pyrogram import Client, filters
+from telegraph import upload_file
+from plugins.helper.get_file_id import get_file_id
+
+TMP_DOWNLOAD_DIRECTORY = "./DOWNLOADS/"
+
+@Client.on_message(
+    filters.command("telegraph") 
+)
+async def telegraph(client, message):
+    koshik = await message.reply_text("**Processing...😪**")
+    replied = message.reply_to_message
+    if not replied:
+        await koshik.edit_text("Reply to a supported media file")
+        return
+    file_info = get_file_id(replied)
+    if not file_info:
+        await koshik.edit_text("Not supported!")
+        return
+    _t = os.path.join(
+        TMP_DOWNLOAD_DIRECTORY,
+        str(replied.message_id)
+    )
+    if not os.path.isdir(_t):
+        os.makedirs(_t)
+    _t += "/"
+    download_location = await replied.download(
+        _t
+    )
+    try:
+        response = upload_file(download_location)
+    except Exception as document:
+        await koshik.edit_text(message, text=document)
+    else:
+        await koshik.edit_text(
+            f"<b>Link :-</b> <code>https://telegra.ph{response[0]}</code>\n\n<b>",
+            disable_web_page_preview=True
+        )
+    finally:
+        shutil.rmtree(
+            _t,
+            ignore_errors=True
+        )
