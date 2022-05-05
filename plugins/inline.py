dc85c477cb71a4840b498a452fa512a4bd2575db
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-05 17:31:17 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -5,6 +5,10 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQue
 from database.ia_filterdb import get_search_results
 from utils import is_subscribed, get_size, temp
 from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
+import os
+import play_scraper
+from pyrogram import Client, filters
+from pyrogram.types import *
 
 logger = logging.getLogger(__name__)
 cache_time = 0 if AUTH_USERS or AUTH_CHANNEL else CACHE_TIME
@@ -108,6 +112,38 @@ def get_reply_markup(query):
         ]
     return InlineKeyboardMarkup(buttons)
 
+async def search(bot, update):
+    results = play_scraper.search(update.query)
+    answers = []
+    for result in results:
+        details = "**Title:** `{}`".format(result["title"]) + "\n" \
+        "**Description:** `{}`".format(result["description"]) + "\n" \
+        "**App ID:** `{}`".format(result["app_id"]) + "\n" \
+        "**Developer:** `{}`".format(result["developer"]) + "\n" \
+        "**Developer ID:** `{}`".format(result["developer_id"]) + "\n" \
+        "**Score:** `{}`".format(result["score"]) + "\n" \
+        "**Price:** `{}`".format(result["price"]) + "\n" \
+        "**Full Price:** `{}`".format(result["full_price"]) + "\n" \
+        "**Free:** `{}`".format(result["free"]) + "\n" \
+        "\n" + "Made by @FayasNoushad"
+        reply_markup = InlineKeyboardMarkup(
+            [[InlineKeyboardButton(text="Play Store", url="https://play.google.com"+result["url"])]]
+        )
+        try:
+            answers.append(
+                InlineQueryResultArticle(
+                    title=result["title"],
+                    description=result.get("description", None),
+                    thumb_url=result.get("icon", None),
+                    input_message_content=InputTextMessageContent(
+                        message_text=details, disable_web_page_preview=True
+                    ),
+                    reply_markup=reply_markup
+                )
+            )
+        except Exception as error:
+            print(error)
+    await update.answer(answers)
 
 
 
