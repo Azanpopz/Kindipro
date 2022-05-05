be4a52c05ed2a1e99a03444359ce9747696bbb2d
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-05 21:42:39 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -5,12 +5,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQue
 from database.ia_filterdb import get_search_results
 from utils import is_subscribed, get_size, temp
 from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
-import os
-import play_scraper
-from pyrogram import Client, filters
-from pyrogram.types import *
-from pyrogram import Client, filters
-from pyrogram import filters
+
 
 
 logger = logging.getLogger(__name__)
@@ -131,36 +126,3 @@ async def inline_users(query: InlineQuery):
 
 
 
-@Client.on_message(filters.command("app"))
-async def apk(client, message):
-    results = play_scraper.apk(client, Message)
-    answers = []
-    for result in results:
-        details = "**Title:** `{}`".format(result["title"]) + "\n" \
-        "**Description:** `{}`".format(result["description"]) + "\n" \
-        "**App ID:** `{}`".format(result["app_id"]) + "\n" \
-        "**Developer:** `{}`".format(result["developer"]) + "\n" \
-        "**Developer ID:** `{}`".format(result["developer_id"]) + "\n" \
-        "**Score:** `{}`".format(result["score"]) + "\n" \
-        "**Price:** `{}`".format(result["price"]) + "\n" \
-        "**Full Price:** `{}`".format(result["full_price"]) + "\n" \
-        "**Free:** `{}`".format(result["free"]) + "\n" \
-        "\n" + "Made by @FayasNoushad"
-        reply_markup = InlineKeyboardMarkup(
-            [[InlineKeyboardButton(text="Play Store", url="https://play.google.com"+result["url"])]]
-        )
-        try:
-            answers.append(
-                InlineQueryResultArticle(
-                    title=result["title"],
-                    description=result.get("description", None),
-                    thumb_url=result.get("icon", None),
-                    input_message_content=InputTextMessageContent(
-                        message_text=details, disable_web_page_preview=True
-                    ),
-                    reply_markup=reply_markup
-                )
-            )
-        except Exception as error:
-            print(error)
-    await client.answer(answers)
