36ba0e31e2c6d94f9c5050da2186220ebf712334
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-06 23:20:18 +0530

    Update group.py

--- plugins/group.py
+++ plugins/group.py
@@ -2,7 +2,80 @@ from googletrans import Translator
 from pyrogram import Client, filters
 from plugins.helper.list import list
 from plugins.helper.database import find_one
-@Client.on_message(filters.group & filters.command(["tr"]))
+
+
+import os
+import re
+import requests
+import traceback
+from asyncio import get_running_loop
+from io import BytesIO
+from .list import list
+from database.gtrans_mdb import find_one
+
+from googletrans import Translator
+
+from gtts import gTTS
+
+
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, Message
+
+
+@Client.on_message(filters.group & filters.command(["t"]))
+async def left(client,message):
+	if (message.reply_to_message):
+		try:
+			lgcd = message.text.split("/tr")
+			lg_cd = lgcd[1].lower().replace(" ", "")
+			tr_text = message.reply_to_message.text
+			translator = Translator()
+			translation = translator.translate(tr_text,dest = lg_cd)
+			hehek = InlineKeyboardMarkup(
+                                [
+                                    [
+                                        InlineKeyboardButton(
+                                            "language codes", url="https://cloud.google.com/translate/docs/languages"
+                                        )
+                                    ],
+				    [
+                                        InlineKeyboardButton(
+                                            "✗ close the translate ✗", callback_data="close_data"
+                                        )
+                                    ],
+                                ]
+                            )
+			try:
+				for i in list:
+					if list[i]==translation.src:
+						fromt = i
+					if list[i] == translation.dest:
+						to = i 
+				await message.reply_text(f"Translated from **{fromt.capitalize()}** To **{to.capitalize()}**\n\n```{translation.text}```", reply_markup=hehek, quote=True)
+			except:
+			   	await message.reply_text(f"Translated from **{translation.src}** To **{translation.dest}**\n\n```{translation.text}```", reply_markup=hehek, quote=True)
+			
+
+		except :
+			print("error")
+	else:
+			 ms = await message.reply_text("You can Use This Command by using reply to message")
+			 await ms.delete()
+				
+async def spaste(content: str):
+    siteurl = "https://spaceb.in/api/v1/documents/"
+    try:
+        resp = requests.post(siteurl, data={"content": content, "extension": "py"})
+        response = resp.json()
+        link = f"https://spaceb.in/{response['payload']['id']}"
+        rawlink = f"{siteurl}{response['payload']['id']}/raw"
+        return link, rawlink
+    except Exception as e:
+        print(e)
+        return None
+
+
+@Client.on_message(filters.command(["tr"]))
 async def left(client,message):
 	if (message.reply_to_message):
 		try:
