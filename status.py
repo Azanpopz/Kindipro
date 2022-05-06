8ce7564d849f90e64b5630a7f48f5d22f8cdc06f
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-06 22:14:29 +0530

    Update status.py

--- status.py
+++ status.py
@@ -4,7 +4,7 @@ class Config(object):
       BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
       API_ID = int(os.environ.get("APP_ID", 12345))
       API_HASH = os.environ.get("API_HASH")
-      CAPTION_STATUS = os.environ.get("CAPTION_STATUS", "")
+      CAPTION = os.environ.get("CAPTION", "")
       CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "nil")
       ADMIN = os.environ.get("ADMIN", "kinzanoufal")
 
