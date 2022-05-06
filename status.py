6e98f14ec27335d130106ff07710a92b3c4bc9e6
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-06 22:11:02 +0530

    Update status.py

--- status.py
+++ status.py
@@ -6,6 +6,6 @@ class Config(object):
       API_HASH = os.environ.get("API_HASH")
       CAPTION_STATUS = os.environ.get("CAPTION_STATUS", "")
       CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "nil")
-      ADMINS = os.environ.get("ADMINS", "kinzanoufal")
+      ADMIN = os.environ.get("ADMIN", "kinzanoufal")
 
 
