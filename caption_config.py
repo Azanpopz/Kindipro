6afc9ccbaa6f20dff06a159c1913fc43b7b4d772
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-06 21:59:51 +0530

    Update caption_config.py

--- caption_config.py
+++ caption_config.py
@@ -9,9 +9,6 @@ class Config(object):
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "MINNAL_MURALI_ROBOT")
 
 
-import os
-
-
 
 class Config(object):
       BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
@@ -96,41 +93,6 @@ __○ ᴄʀᴇᴀᴛᴏʀ :  @AvishkarPatil__
 
 ○ <i>SOURCE</i> : <b><a href="https://github.com/avipatilpro/Caption-Bot">Caption Bot</a></b>  
 """
-
-
-
-
-
-import pyrogram
-
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
-import os
-
-from caption_config import Config
-from pyrogram import Client 
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
-class autocaption(Client):
-    
-    def __init__(self):
-        super().__init__(
-            session_name="Captioner",
-            bot_token = Config.BOT_TOKEN,
-            api_id = Config.API_ID,
-            api_hash = Config.API_HASH,
-            workers = 20,
-            plugins = dict(
-                root="Plugins"
-            )
-        )
-
-if __name__ == "__main__" :
-    
-
 
 
 
