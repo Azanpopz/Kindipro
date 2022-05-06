48499aa10df5cbc41d332bc95cb0ea131f59a6e0
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-06 22:05:42 +0530

    Update caption_config.py

--- caption_config.py
+++ caption_config.py
@@ -14,7 +14,7 @@ class Config(object):
       BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
       API_ID = int(os.environ.get("APP_ID", 12345))
       API_HASH = os.environ.get("API_HASH")
-      CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "")
+      CAPTION_STATUS = os.environ.get("CAPTION_STATUS", "")
       CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "nil")
       ADMINS = os.environ.get("ADMINS", "kinzanoufal")
 
