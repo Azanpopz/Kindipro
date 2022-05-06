ed1d4e5cbfb721a7fa4e0b54a760ab70b325a288
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-06 22:00:38 +0530

    Create caption_status.py

---
+++ caption_status.py
@@ -0,0 +1,35 @@
+import pyrogram
+
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+
+from caption_config import Config
+from pyrogram import Client 
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+class autocaption(Client):
+    
+    def __init__(self):
+        super().__init__(
+            session_name="Captioner",
+            bot_token = Config.BOT_TOKEN,
+            api_id = Config.API_ID,
+            api_hash = Config.API_HASH,
+            workers = 20,
+            plugins = dict(
+                root="Plugins"
+            )
+        )
+
+if __name__ == "__main__" :
+    
+
+
+
+
+
+
