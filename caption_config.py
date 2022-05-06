d7136ec97262f03466f49bcd099ace37b91d4497
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-06 21:58:07 +0530

    Update caption_config.py

--- caption_config.py
+++ caption_config.py
@@ -9,6 +9,19 @@ class Config(object):
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "MINNAL_MURALI_ROBOT")
 
 
+import os
+
+
+
+class Config(object):
+      BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+      API_ID = int(os.environ.get("APP_ID", 12345))
+      API_HASH = os.environ.get("API_HASH")
+      CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "")
+      CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "nil")
+      ADMINS = os.environ.get("ADMINS", "kinzanoufal")
+
+
 class Translation(object):
 
       
