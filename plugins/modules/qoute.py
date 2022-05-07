ee3293a607b673578e9036d8c0ad8f89b3abb4b2
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-07 20:04:48 +0530

    Update qoute.py

--- plugins/modules/qoute.py
+++ plugins/modules/qoute.py
@@ -188,7 +188,7 @@ async def create_sticker(c: Client, m: Message):
     font_who = ImageFont.truetype("TitilliumWeb-Bold.ttf", 24)
     AKKU = ImageFont.truetype("Sticky-Notes.ttf", body_font_size)
 
-    img = Image.new("RGBA", (512, 0), (255, 255, 255, 0))
+    img = Image.new("RGBA", (512, 255), (255, 255, 255, 0))
     draw = ImageDraw.Draw(img)
     draw.rounded_rectangle = rounded_rectangle
 
