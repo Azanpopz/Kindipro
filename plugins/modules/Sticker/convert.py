164f9dfac0563f5f67125c9291a2e82b5f0237ca
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-08 01:33:32 +0530

    Update convert.py

--- plugins/modules/Sticker/convert.py
+++ plugins/modules/Sticker/convert.py
@@ -18,34 +18,34 @@ async def sng(bot, message):
         if not message.reply_to_message:
           await message.reply_text("Please reply to a message")
         else:          
-          message = await msg.reply("Converting...")
-          image = await msg.download(file_name=f"{name_format}.jpg")
-          user_id = msg.from_user.id
-          message_id = msg.message_id
+          message = await message.reply("Converting...")
+          image = await message.download(file_name=f"{name_format}.jpg")
+          user_id = message.from_user.id
+          message_id = message.message_id
           name_format = f"StarkBots_{user_id}_{message_id}"
           await mee.delete()
           
-    if msg.photo:
-        message = await msg.reply("Converting...")
-        image = await msg.download(file_name=f"{name_format}.jpg")
-        await message.edit("Sending...")
-        im = Image.open(image).convert("RGB")
-        im.save(f"{name_format}.webp", "webp")
-        sticker = f"{name_format}.webp"
-        await msg.reply_sticker(sticker)
-        await message.delete()
-        os.remove(sticker)
-        os.remove(image)
-    elif msg.sticker.is_animated:
-        await msg.reply("Animated stickers are not supported !", quote=True)
-    else:
-        message = await msg.reply("Converting...")
-        sticker = await msg.download(file_name=f"{name_format}.webp")
-        await message.edit("Sending...")
-        im = Image.open(sticker).convert("RGB")
-        im.save(f"{name_format}.jpg", "jpeg")
-        image = f"{name_format}.jpg"
-        await msg.reply_photo(image)
-        await message.delete()
-        os.remove(image)
-        os.remove(sticker)
+      if message.photo:
+          message = await message.reply("Converting...")
+          image = await message.download(file_name=f"{name_format}.jpg")
+          await message.edit("Sending...")
+          im = Image.open(image).convert("RGB")
+          im.save(f"{name_format}.webp", "webp")
+          sticker = f"{name_format}.webp"
+          await message.reply_sticker(sticker)
+          await message.delete()
+          os.remove(sticker)
+          os.remove(image)
+      elif msg.sticker.is_animated:
+          await msg.reply("Animated stickers are not supported !", quote=True)
+      else:
+          message = await message.reply("Converting...")
+          sticker = await message.download(file_name=f"{name_format}.webp")
+          await message.edit("Sending...")
+          im = Image.open(sticker).convert("RGB")
+          im.save(f"{name_format}.jpg", "jpeg")
+          image = f"{name_format}.jpg"
+          await message.reply_photo(image)
+          await message.delete()
+          os.remove(image)
+          os.remove(sticker)
