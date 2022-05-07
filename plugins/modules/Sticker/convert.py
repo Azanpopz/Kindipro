8fc5d53b018b59c8cccf996990156039cc3386bb
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-08 01:25:58 +0530

    Update convert.py

--- plugins/modules/Sticker/convert.py
+++ plugins/modules/Sticker/convert.py
@@ -24,7 +24,7 @@ async def sng(bot, message):
           rpl = lyrics(song)
           await mee.delete()
           try:
-          user_id = msg.from_user.id
+              user_id = msg.from_user.id
           message_id = msg.message_id
           name_format = f"StarkBots_{user_id}_{message_id}"
     if msg.photo:
