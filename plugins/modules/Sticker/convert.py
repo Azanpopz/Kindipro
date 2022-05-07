2055c1c7bfed33418341a1887692695fce1012dd
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-07 23:18:39 +0530

    Update convert.py

--- plugins/modules/Sticker/convert.py
+++ plugins/modules/Sticker/convert.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message
 from pyrogram import Client, filters
 
 
-@Client.on_message(filters.private & filters.incoming & (filters.sticker | filters.photo))
+@Client.on_message(filters.command(['/convert']))
 async def sticker_image(_, msg: Message):
     user_id = msg.from_user.id
     message_id = msg.message_id
