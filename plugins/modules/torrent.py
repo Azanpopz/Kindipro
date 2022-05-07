1c9571a1fbd3f104b758ca116b7a38f914139973
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-07 15:06:55 +0530

    Update torrent.py

--- plugins/modules/torrent.py
+++ plugins/modules/torrent.py
@@ -7,7 +7,10 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, 
 from configs import Config
 from tool import SearchYTS, SearchAnime, Search1337x, SearchPirateBay
 
-TorrentBot = Client(session_name=Config.SESSION_NAME, api_id=Config.API_ID, api_hash=Config.API_HASH, bot_token=Config.BOT_TOKEN)
+
+
+app = Client("trntsrcbot", api_id=int(os.environ.get("API_ID")), api_hash=os.environ.get("API_HASH"), bot_token=os.environ.get("BOT_TOKEN"))
+
 DEFAULT_SEARCH_MARKUP = [
                     [InlineKeyboardButton("𝗦𝗲𝗮𝗿𝗰𝗵 𝗬𝗧𝗦 𝗠𝗼𝘃𝗶𝗲𝘀 📺🔥", switch_inline_query_current_chat="YTS "),
                      InlineKeyboardButton("𝗦𝗲𝗮𝗿𝗰𝗵 𝗜𝗻 1337x 🔥", switch_inline_query_current_chat="")],
@@ -20,7 +23,7 @@ DEFAULT_SEARCH_MARKUP = [
 
 
 
-@TorrentBot.on_inline_query()
+@Client.on_inline_query()
 async def inline_handlers(_, inline: InlineQuery):
     search_ts = inline.query
     answers = []
