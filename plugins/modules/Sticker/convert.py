a14ed7fe1e49b7d680d9dd2d2bf833d99f97b2a4
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-08 00:40:50 +0530

    Update convert.py

--- plugins/modules/Sticker/convert.py
+++ plugins/modules/Sticker/convert.py
@@ -25,8 +25,7 @@ async def sticker_image(_, msg: Message):
             await message.delete()
             os.remove(sticker)
             os.remove(image)
-        elif msg.sticker.is_animated:
-            await msg.reply("Animated stickers are not supported !", quote=True)
+        
         else:
             message = await msg.reply("Converting...")
             sticker = await msg.download(file_name=f"{name_format}.webp")
