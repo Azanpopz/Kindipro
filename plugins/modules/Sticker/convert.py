f89479fe2d1a76706cc63453d09fa95fc7ceaf8c
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-08 00:33:36 +0530

    Update convert.py

--- plugins/modules/Sticker/convert.py
+++ plugins/modules/Sticker/convert.py
@@ -12,17 +12,17 @@ async def sticker_image(_, msg: Message):
         
         message = await msg.reply("Converting...")
         if msg.photo:
-        user_id = msg.from_user.id
-        message_id = msg.message_id
-        name_format = f"StarkBots_{user_id}_{message_id}"
+            user_id = msg.from_user.id
+            message_id = msg.message_id
+            name_format = f"StarkBots_{user_id}_{message_id}"
     
-        image = await msg.download(file_name=f"{name_format}.jpg")
-        await message.edit("Sending...")
-        im = Image.open(image).convert("RGB")
-        im.save(f"{name_format}.webp", "webp")
-        sticker = f"{name_format}.webp"
-        await msg.reply_sticker(sticker)
-        await message.delete()
-        os.remove(sticker)
-        os.remove(image)
-    
+            image = await msg.download(file_name=f"{name_format}.jpg")
+            await message.edit("Sending...")
+            im = Image.open(image).convert("RGB")
+            im.save(f"{name_format}.webp", "webp")
+            sticker = f"{name_format}.webp"
+            await msg.reply_sticker(sticker)
+            await message.delete()
+            os.remove(sticker)
+            os.remove(image)
+      
