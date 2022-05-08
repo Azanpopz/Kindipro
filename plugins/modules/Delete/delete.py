dbd45190a15fb8caed7b4f6c2cbafcca4c2a7e78
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-08 19:04:44 +0530

    Create delete.py

---
+++ plugins/modules/Delete/delete.py
@@ -0,0 +1,37 @@
+import asyncio
+from os import environ
+from pyrogram import Client, filters, idle
+
+API_ID = int(environ.get("API_ID"))
+API_HASH = environ.get("API_HASH")
+BOT_TOKEN = environ.get("BOT_TOKEN")
+SESSION = environ.get("SESSION")
+TIME = int(environ.get("TIME"))
+GROUPS = []
+for grp in environ.get("GROUPS").split():
+    GROUPS.append(int(grp))
+ADMINS = []
+for usr in environ.get("ADMINS").split():
+    ADMINS.append(int(usr))
+
+START_MSG = "<b>Hai {},\nI'm a simple bot to delete group messages after a specific time <a href=https://github.com/jkdevil27/MsgAutoDELETE>Make your own</a> </b>"
+
+
+
+
+@Client.on_message(filters.command('dt') & filters.private)
+async def dt(bot, message):
+    await message.reply(START_MSG.format(message.from_user.mention))
+
+@Client.on_message(filters.chat(GROUPS))
+async def delete(user, message):
+    try:
+       if message.from_user.id in ADMINS:
+          return
+       else:
+          await asyncio.sleep(TIME)
+          await Bot.delete_messages(message.chat.id, message.id)
+    except Exception as e:
+       print(e)
+       
+
