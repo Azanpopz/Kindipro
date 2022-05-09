3d13b4b3ae1e35ee6bdfff55ef7dbb3f046aff06
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-09 18:32:37 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -1,105 +1,52 @@
-"""
-RadioPlayerV3, Telegram Voice Chat Bot
-Copyright (c) 2021  Asm Safone <https://github.com/AsmSafone>
+import os
+import play_scraper
+from pyrogram import Client, filters
+from pyrogram.types import *
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
+Bot = Client(
+    "Play-Store-Bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
 
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>
-"""
 
-import asyncio
-from Config import Configs
-
-from pyrogram import Client, errors
-from youtubesearchpython import VideosSearch
-from pyrogram.handlers import InlineQueryHandler
-from pyrogram.types import InlineQueryResultArticle, InlineQueryResultPhoto, InputTextMessageContent, InlineKeyboardButton, InlineKeyboardMarkup
-
-REPLY_MESSAGE=Configs.REPLY_MESSAGE
-
-buttons = [
-            [
-                InlineKeyboardButton("❔ HOW TO USE ME ❔", callback_data="help"),
-            ],
-            [
-                InlineKeyboardButton("CHANNEL", url="https://t.me/AsmSafone"),
-                InlineKeyboardButton("SUPPORT", url="https://t.me/AsmSupport"),
-            ],
-            [
-                InlineKeyboardButton("🤖 MAKE YOUR OWN BOT 🤖", url="https://heroku.com/deploy?template=https://github.com/AsmSafone/RadioPlayerV3"),
-            ]
-         ]
 
 
 @Client.on_inline_query()
-async def search(client, query):
+async def search(bot, update):
+    results = play_scraper.search(update.query)
     answers = []
-    if query.query == "SAF_ONE":
-        answers.append(
-            InlineQueryResultPhoto(
-                title="Deploy Your Own Radio Player",
-                thumb_url="https://telegra.ph/file/4e839766d45935998e9c6.jpg",
-                photo_url="https://telegra.ph/file/4e839766d45935998e9c6.jpg",
-                caption=f"{REPLY_MESSAGE}\n\n<b>© Powered By : \n@AsmSafone | @AsmSupport 👑</b>",
-                reply_markup=InlineKeyboardMarkup(buttons)
-                )
-            )
-        await query.answer(results=answers, cache_time=0)
-        return
-    string = query.query.lower().strip().rstrip()
-    if string == "":
-        await client.answer_inline_query(
-            query.id,
-            results=answers,
-            switch_pm_text=("✍️ Type An Video Name !"),
-            switch_pm_parameter="help",
-            cache_time=0
+    for result in results:
+        details = "**Title:** `{}`".format(result["title"]) + "\n" \
+        "**Description:** `{}`".format(result["description"]) + "\n" \
+        "**App ID:** `{}`".format(result["app_id"]) + "\n" \
+        "**Developer:** `{}`".format(result["developer"]) + "\n" \
+        "**Developer ID:** `{}`".format(result["developer_id"]) + "\n" \
+        "**Score:** `{}`".format(result["score"]) + "\n" \
+        "**Price:** `{}`".format(result["price"]) + "\n" \
+        "**Full Price:** `{}`".format(result["full_price"]) + "\n" \
+        "**Free:** `{}`".format(result["free"]) + "\n" \
+        "\n" + "Made by @FayasNoushad"
+        reply_markup = InlineKeyboardMarkup(
+            [[InlineKeyboardButton(text="Play Store", url="https://play.google.com"+result["url"])]]
         )
-    else:
-        videosSearch = VideosSearch(string.lower(), limit=50)
-        for v in videosSearch.result()["result"]:
+        try:
             answers.append(
                 InlineQueryResultArticle(
-                    title=v["title"],
-                    description=("Duration: {} Views: {}").format(
-                        v["duration"],
-                        v["viewCount"]["short"]
-                    ),
+                    title=result["title"],
+                    description=result.get("description", None),
+                    thumb_url=result.get("icon", None),
                     input_message_content=InputTextMessageContent(
-                        "/play https://www.youtube.com/watch?v={}".format(
-                            v["id"]
-                        )
+                        message_text=details, disable_web_page_preview=True
                     ),
-                    thumb_url=v["thumbnails"][0]["url"]
+                    reply_markup=reply_markup
                 )
             )
-        try:
-            await query.answer(
-                results=answers,
-                cache_time=0
-            )
-        except errors.QueryIdInvalid:
-            await query.answer(
-                results=answers,
-                cache_time=0,
-                switch_pm_text=("Error: Search Timed Out!"),
-                switch_pm_parameter="",
-            )
+        except Exception as error:
+            print(error)
+    await update.answer(answers)
 
 
-__handlers__ = [
-    [
-        InlineQueryHandler(
-            search
-        )
-    ]
-]
+
