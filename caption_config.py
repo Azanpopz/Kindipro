fb82fc5046ad5338da6b7bd5c810e889819d95a4
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-09 13:28:32 +0530

    Update caption_config.py

--- caption_config.py
+++ caption_config.py
@@ -1,8 +1,8 @@
 import os
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH", "")
+    BT_TOKEN = os.environ.get("BT_TOKEN", "5199038181:AAGJIsXbVjtAiwh0yS4Ir3Q0sktjzIGzro8")
+    ID = int(os.environ.get("ID", "1778836"))
+    HASH = os.environ.get("HASH", "7bcf61fcd32b8652cd5876b02dcf57ae")
     CAPTION = os.environ.get("CAPTION", "╔════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ═════╗\n♻️ 𝙅𝙊𝙄𝙉 :- @nasrani_update\n♻️ 𝙅𝙊𝙄𝙉 :- @NasraniSeries\n╚════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ═════╝")
     BUTTON_TEXT = os.environ.get("BUTTON", "🔻Join Channel🔻")
     URL_LINK = os.environ.get("LINK", "T.ME/Nasrani_update")
