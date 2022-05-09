a33c3b3ea5495dcc5bbbe693cd28962c8c28d9c9
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-09 17:17:38 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -90,7 +90,7 @@ async def answer(bot, query):
         if f_caption is None:
             f_caption = f"{file.file_name}"
         results.append(
-            InlineQueryResultCachedMedia(
+            InlineQueryResultCachedDocument(
                 photo_url="https://telegra.ph/file/4e839766d45935998e9c6.jpg",
                 title=file.file_name,
                 file_id=file.file_id,
