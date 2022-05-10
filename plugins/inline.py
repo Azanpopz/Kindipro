4d15d641e784606f6093cd0f7c66cad35faf4641
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-10 11:35:49 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -1,52 +1,103 @@
+# Author: Fayas (https://github.com/FayasNoushad) (@FayasNoushad)
+
 import os
-import play_scraper
+import requests
+from requests.utils import requote_uri
 from pyrogram import Client, filters
 from pyrogram.types import *
 
+API = "https://api.abirhasan.wtf/google?query="
+
 
 Bot = Client(
-    "Play-Store-Bot",
+    "Google-Search-Bot",
     bot_token = os.environ["BOT_TOKEN"],
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"]
 )
 
 
+START_TEXT = """Hello {}
+I am a google search bot.
+
+> `I can search from google. Use me in inline.`
+
+Made by @FayasNoushad"""
+
+JOIN_BUTTON = [
+    InlineKeyboardButton(
+        text='⚙ Join Updates Channel ⚙',
+        url='https://telegram.me/nasrani_update'
+    )
+]
 
 
-@Client.on_inline_query()
-async def search(bot, update):
-    results = play_scraper.search(update.query)
+@Bot.on_message(filters.private & filters.command(["start"]))
+async def start(bot, update):
+    await update.reply_text(
+        text=START_TEXT.format(update.from_user.mention),
+        reply_markup=InlineKeyboardMarkup([JOIN_BUTTON]),
+        disable_web_page_preview=True,
+        quote=True
+    )
+
+
+@Bot.on_message(filters.private & filters.text)
+async def filter(bot, update):
+    await update.reply_text(
+        text="`Click the button below for searching...`",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton(text="Search Here", switch_inline_query_current_chat=update.text)],
+                [InlineKeyboardButton(text="Search in another chat", switch_inline_query=update.text)]
+            ]
+        ),
+        disable_web_page_preview=True,
+        quote=True
+    )
+
+
+@Bot.on_inline_query()
+async def inline(bot, update):
+    results = google(update.query)
     answers = []
     for result in results:
-        details = "**Title:** `{}`".format(result["title"]) + "\n" \
-        "**Description:** `{}`".format(result["description"]) + "\n" \
-        "**App ID:** `{}`".format(result["app_id"]) + "\n" \
-        "**Developer:** `{}`".format(result["developer"]) + "\n" \
-        "**Developer ID:** `{}`".format(result["developer_id"]) + "\n" \
-        "**Score:** `{}`".format(result["score"]) + "\n" \
-        "**Price:** `{}`".format(result["price"]) + "\n" \
-        "**Full Price:** `{}`".format(result["full_price"]) + "\n" \
-        "**Free:** `{}`".format(result["free"]) + "\n" \
-        "\n" + "Made by @FayasNoushad"
-        reply_markup = InlineKeyboardMarkup(
-            [[InlineKeyboardButton(text="Play Store", url="https://play.google.com"+result["url"])]]
-        )
-        try:
-            answers.append(
-                InlineQueryResultArticle(
-                    title=result["title"],
-                    description=result.get("description", None),
-                    thumb_url=result.get("icon", None),
-                    input_message_content=InputTextMessageContent(
-                        message_text=details, disable_web_page_preview=True
-                    ),
-                    reply_markup=reply_markup
+        answers.append(
+            InlineQueryResultArticle(
+                title=result["title"],
+                description=result["description"],
+                input_message_content=InputTextMessageContent(
+                    message_text=result["text"],
+                    disable_web_page_preview=True
+                ),
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [InlineKeyboardButton(text="Link", url=result["link"])],
+                        JOIN_BUTTON
+                    ]
                 )
             )
-        except Exception as error:
-            print(error)
+        )
     await update.answer(answers)
+
+
+def google(query):
+    r = requests.get(API + requote_uri(query))
+    informations = r.json()["results"][:50]
+    results = []
+    for info in informations:
+        text = f"**Title:** `{info['title']}`"
+        text += f"\n**Description:** `{info['description']}`"
+        text += f"\n\nMade by @FayasNoushad"
+        results.append(
+            {
+                "title": info['title'],
+                "description": info['description'],
+                "text": text,
+                "link": info['link']
+            }
+        )
+    return results
 
 
 
