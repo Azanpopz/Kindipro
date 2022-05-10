18570c9fec36d3363c51901c896ded6e18de4fa4
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-10 23:27:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,6 +16,33 @@ from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
 
 from utils import temp
 
+
+from pyrogram import Client
+import os
+
+TOKEN = os.environ.get("TOKEN", "")
+
+APP_ID = int(os.environ.get("APP_ID", ""))
+
+API_HASH = os.environ.get("API_HASH", "")
+
+if __name__ == "__main__" :
+    plugins = dict(
+        root="plugins"
+    )
+    app = Client(
+        "renamer",
+        bot_token=TOKEN,
+        api_id=APP_ID,
+        api_hash=API_HASH,
+        plugins=plugins
+    )
+
+
+
+
+
+
 class Bot(Client):
 
     def __init__(self):
