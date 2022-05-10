87141a07674f412cd89bfe55ae2072963206ec53
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-10 18:29:48 +0530

    Create inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -17,13 +17,28 @@ API = "https://apibu.herokuapp.com/api/y-images?query="
 
 @Client.on_message(filters.command(["img"]) & filters.private & filters.text & filters.photo)
 async def search(bot, update):
-    results = requests.get(API + requests.utils.requote_uri(update.query)).json()["result"][:1]
-    answers = []
+    for result in results:
+        details = "**Title:** `{}`".format(result["title"]) + "\n" \
+        "**Description:** `{}`".format(result["description"]) + "\n" \
+        "**App ID:** `{}`".format(result["app_id"]) + "\n" \
+        "**Developer:** `{}`".format(result["developer"]) + "\n" \
+        "**Developer ID:** `{}`".format(result["developer_id"]) + "\n" \
+        "**Score:** `{}`".format(result["score"]) + "\n" \
+        "**Price:** `{}`".format(result["price"]) + "\n" \
+        "**Full Price:** `{}`".format(result["full_price"]) + "\n" \
+        "**Free:** `{}`".format(result["free"]) + "\n" \
+        "\n" + "Made by @FayasNoushad"
+        reply_markup = InlineKeyboardMarkup(
+            [[InlineKeyboardButton(text="Play Store", url="https://play.google.com"+result["url"])]]
+        )
+        try:
     await update.reply_photo(
-        title=update.bot.capitalize(),
-        description=result,
-        caption="Made by @FayasNoushad",
-        photo_url=result,
+        title=result["title"],
+        description=result.get("description", None),
+        thumb_url=result.get("icon", None),
+        input_message_content=InputTextMessageContent(
+        message_text=details, disable_web_page_preview=True
+                  
         reply_markup=InlineKeyboardMarkup(
             [
                 [InlineKeyboardButton(text="Search Here", switch_inline_query_current_chat=update.text)],
@@ -34,9 +49,11 @@ async def search(bot, update):
         quote=True
     )
 
+        except Exception as error:
+            print(error)
+    await update.answer(answers)
 
-
-    await update.reply_photo(answers)
+    
 
 
 
