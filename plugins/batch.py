7cd39491383351eb027396e24fbfd5f8c3abc379
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-11 17:45:25 +0530

    Update batch.py

--- plugins/batch.py
+++ plugins/batch.py
@@ -1,8 +1,8 @@
 import asyncio
 import json
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-from config import BATCH, ADMINS, SOURCE_CODE
-from util import replace_mdisk_link, caption
+from config import CHANNELS, ADMINS, SOURCE_CODE
+from utils import replace_mdisk_link, caption
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 import os
 import sys
@@ -26,13 +26,13 @@ cancel_button = [[
 channel_id = ""
 
 
-@Client.on_message(filters.private & filters.command('batch'))
+@Client.on_message(filters.private & ~filters.edited & filters.command('batch'))
 async def batch(c, m):
     if m.from_user.id in ADMINS:
 
         global channel_id
         print("Started")
-        if BATCH is True:
+        if CHANNELS is True:
             if len(m.command) < 2:
                 await m.reply_text(BATCH)
             else:
@@ -54,8 +54,8 @@ async def batch(c, m):
                               reply_markup=InlineKeyboardMarkup(buttons))
                 print(channel_id)
 
-        elif BATCH is False:
-            await m.reply(text="Set your BATCH var to True in HEROKU to use this command")
+        elif CHANNELS is False:
+            await m.reply(text="Set your CHANNELS var to True in HEROKU to use this command")
     elif m.from_user.id not in ADMINS:
         await m.reply_text(
                 f"""This bot works only for ADMINS of this bot. Make your own Bot.\n\n[Source Code]({SOURCE_CODE})""")
@@ -68,19 +68,29 @@ async def cancel(c, m):
     if m.data == "cancel":
         await m.message.delete()
     elif m.data == "batch":
-        if BATCH is True:
+        if CHANNELS is True:
             try:
                 txt = await c.send_message(channel_id, ".")
                 await txt.delete()
-                print(txt.id)
+                print(txt.message_id)
             except ChatWriteForbidden:
                 await m.message.edit("Bot is not an admin in the given channel")
             start_msg = await m.message.edit(text=f"Batch Shortening Started!\n\n Channel: {channel_id}\n\nTo Cancel /cancel",
                                  )
 
-            for i in range(txt.id, 1, -1):
+            for i in range(txt.message_id, 1, -1):
                 try:
                     message = await c.get_messages(channel_id, i)
+
+                    if message.text:
+                        txt = message.text
+                        ent = await caption(message.entities)
+                        print(ent)
+                    elif message.caption:
+                        txt = message.caption
+                        ent = await caption(message.caption_entities)
+
+                    # reply markup - button post
 
                     if message.forward_from:
                         return
@@ -101,18 +111,18 @@ async def cancel(c, m):
                         try:
                             if message.text:
                                 txt = await replace_mdisk_link(txt)
-                                await message.edit_text(text=txt, reply_markup=InlineKeyboardMarkup(buttsons))
+                                await message.edit_text(text=txt, reply_markup=InlineKeyboardMarkup(buttsons), entities=ent)
                             elif message.caption:
                                 txt = await replace_mdisk_link(message.caption)
                                 if message.photo:
                                     await message.edit_caption(photo=message.photo.file_id, caption=txt,
                                                                reply_markup=InlineKeyboardMarkup(buttsons),
-
+                                                               caption_entities=ent
                                                                )
                                 elif message.document:
                                     await message.edit_caption(photo=message.document.file_id, caption=txt,
                                                                reply_markup=InlineKeyboardMarkup(buttsons),
-                                                               )
+                                                               caption_entities=ent)
                         except Exception as e:
                             print(e)
 
@@ -121,7 +131,7 @@ async def cancel(c, m):
                     elif message.text:
                         text = message.text
                         text = await replace_mdisk_link(text)
-                        await message.edit_text(text)
+                        await message.edit_text(text, entities=ent)
 
                     # For media or document messages
 
@@ -131,7 +141,7 @@ async def cancel(c, m):
                         if link == text:
                             print("The given link is either excluded domain link or a droplink link")
                         else:
-                            await message.edit_caption(link)
+                            await message.edit_caption(link, caption_entities=ent)
                     await asyncio.sleep(1)
 
                 except Exception as e:
