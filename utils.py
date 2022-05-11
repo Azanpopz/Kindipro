f72d9cb2b821ca4bdba37dcc5f631fcb3d7f38ad
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-11 14:45:47 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -14,6 +14,16 @@ from database.users_chats_db import db
 from bs4 import BeautifulSoup
 import requests
 
+from config import MDISK_API
+import re
+import requests
+from pyrogram.types import MessageEntity
+import ast
+from pyrogram.types.list import List
+
+
+
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
@@ -380,3 +390,65 @@ def humanbytes(size):
         size /= power
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+
+
+
+
+
+
+####################  Mdisk  ####################
+
+async def get_mdisk(link):
+	url = 'https://diskuploader.mypowerdisk.com/v1/tp/cp'
+	param = {'token': MDISK_API, 'link': link
+			 }
+	res = requests.post(url, json=param)
+	try:
+		shareLink = res.json()
+		link = shareLink["sharelink"]
+	except:
+		print(link, " is invalid")
+	return link
+
+
+async def replace_mdisk_link(text):
+	links = re.findall(r'https?://mdisk.me[^\s]+', text)
+	for link in links:
+		try:
+			mdisk_link = await get_mdisk(link)
+			text = text.replace(link, mdisk_link)
+		except:
+			text = text.replace(link, text)
+
+	return text
+
+
+# caption entities
+
+
+async def caption(caption_entities):
+	print(caption_entities)
+	x = []
+	string = str(caption_entities)
+	res = ast.literal_eval(string)
+	try:
+		for i in res:
+			print(i)
+			if "url" in i:
+				x.append(
+						MessageEntity(type=i["type"], offset=i["offset"], length=i["length"],
+									  url=await get_mdisk(i["url"])))
+			elif "user" in i:
+
+				x.append(MessageEntity(type=i["type"], offset=i["offset"], length=i["length"], url=i["user"]))
+			else:
+
+				x.append(MessageEntity(type=i["type"], offset=i["offset"], length=i["length"]))
+
+		entities = List(x)
+
+	except Exception as e:
+		print(e)
+		entities = caption_entities
+
+	return entities
