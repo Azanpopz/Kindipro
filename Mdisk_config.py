3e70e598cbec99fa17aa50be74f40a3d56201cad
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-11 14:47:45 +0530

    Create Mdisk_config.py

---
+++ Mdisk_config.py
@@ -0,0 +1,11 @@
+import os
+
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+MDISK_API = os.environ.get("MDISK_API")
+CHANNEL_ID = list(int(i.strip()) for i in os.environ.get("CHANNEL_ID").split(" ")) if os.environ.get("CHANNEL_ID") else []
+FORWARD_MESSAGE = bool(os.environ.get("FORWARD_MESSAGE"))
+ADMINS = list(int(i.strip()) for i in os.environ.get("ADMINS").split(",")) if os.environ.get("ADMINS") else []
+SOURCE_CODE = "https://github.com/kevinnadar22/URL-Shortener-V2"
+CHANNELS = bool(os.environ.get("CHANNELS"))
