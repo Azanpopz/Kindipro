1fe8efd5b6b44c3a6e7bd9405dc6dbcd61d80b07
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-11 20:13:50 +0530

    Update thumbfuc.py

--- plugins/rename/thumbfuc.py
+++ plugins/rename/thumbfuc.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from database.Database import find, delthumb, addthumb
 
-@Client.on_message(filters.private & filters.command(['viewthumb']))
+@Client.on_message(filters.command(['viewthumb']))
 async def viewthumb(client,message):
 		thumb = find(int(message.chat.id))
 		if thumb :
@@ -10,12 +10,12 @@ async def viewthumb(client,message):
 			await message.reply_text("**You dont have any custom Thumbnail**")
 	
 	
-@Client.on_message(filters.private & filters.command(['delthumb']))
+@Client.on_message(filters.command(['delthumb']))
 async def removethumb(client,message):
 	delthumb(int(message.chat.id))
 	await message.reply_text("**Custom Thumbnail Deleted Successfully**")
 
-@Client.on_message(filters.private & filters.photo & filters.command(['addthumb']))
+@Client.on_message(filters.group & filters.photo & filters.command(['addthumb']))
 async def addthumbs(client,message):
 	file_id = str(message.photo.file_id)
 	addthumb(message.chat.id , file_id)
