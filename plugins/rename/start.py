6ae14c4d0486a641d50cabed1c1e7e1f25391594
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-11 20:07:12 +0530

    Update start.py

--- plugins/rename/start.py
+++ plugins/rename/start.py
@@ -2,9 +2,8 @@ from pyrogram import Client, filters
 from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
 import humanize
 from database.Database import  insert 
-from config import CHANNEL_ID
 
-@Client.on_message(filters.command(["r"]))
+@Client.on_message(filters.private & filters.command(["t"]))
 async def start(client,message):
 	insert(int(message.chat.id))
 	await message.reply_text(text =f"""
@@ -12,13 +11,18 @@ async def start(client,message):
 	__I am file renamer bot, Please sent any telegram 
 	**Document Or Video** and enter new filenameto rename it__
 	""",reply_to_message_id = message.message_id ,  
-	reply_markup=InlineKeyboardMarkup(
-	 [[ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/lntechnical") ], 
-	[InlineKeyboardButton("Subscribe 🧐", url="https://youtube.com/c/LNtechnical") ]  ]))
+	reply_markup=InlineKeyboardMarkup([[
+          InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/Mo_Tech_Group"), 
+	  InlineKeyboardButton("Subscribe 🧐", url="https://youtube.com/c/MoTech_YT")
+          ],[
+          InlineKeyboardButton("🧩 Deploy 🧩", url="https://youtu.be/7ALYUk-rYMc")
+          ]]
+          )
+        )
 
 
 
-@Client.on_message( filters.chat(CHANNEL_ID) & filters.command(["rr"]))
+@Client.on_message(filters.private &( filters.document | filters.audio | filters.video ))
 async def send_doc(client,message):
        media = await client.get_messages(message.chat.id,message.message_id)
        file = media.document or media.video or media.audio 
