54a58c66a2955ae72dbaae18966b90b96e7843e1
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-11 13:36:17 +0530

    Update caption_config.py

--- caption_config.py
+++ caption_config.py
@@ -9,4 +9,15 @@ class Config(object):
     BT_USERNAME = os.environ.get("BT_USERNAME", "LIZZA_CAPTION_BOT")
 
 
+class mdisk(object):
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
+    API_ID = int(os.environ.get("API_ID", 123456))
+
+    API_HASH = os.environ.get("API_HASH", "")
+    
+    API_KEY = os.environ.get("API_KEY", "tHRFNVu8CkjkdstzXNsp")
+
+    # AUTH_USERS = set(str(x) for x in os.environ.get("AUTH_USERS", "").split())
+
+    # PRIVATE = bool(os.environ.get("PRIVATE", ""))
