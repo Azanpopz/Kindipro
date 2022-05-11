6d4950761297edb0cd0882d3d2b4029528246d75
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-11 17:54:52 +0530

    Update channels.py

--- plugins/channels.py
+++ plugins/channels.py
@@ -1,6 +1,6 @@
 import json
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
-from config import CHANNEL_ID, BATCH
+from config import CHANNEL_ID, B
 from pyrogram import Client, filters
 from util import replace_mdisk_link, caption
 
@@ -11,16 +11,18 @@ import re
 
 
 @Client.on_message(filters.chat(CHANNEL_ID) & (
-        filters.channel | filters.group) & filters.incoming & ~filters.private &
+        filters.channel | filters.group) & filters.incoming & ~filters.edited & ~filters.private &
                    ~filters.forwarded)
 async def channel_link_handler(bot, message: Message):
-    if BATCH is True:
+    if B is True:
 
         if message.text:
             txt = message.text
-
+            ent = await caption(message.entities)
+            print(ent)
         elif message.caption:
             txt = message.caption
+            ent = await caption(message.caption_entities)
 
             # reply markup - button post
 
@@ -41,23 +43,23 @@ async def channel_link_handler(bot, message: Message):
                 txt = await replace_mdisk_link(txt)
                 await message.edit(text=txt,
                                    reply_markup=InlineKeyboardMarkup(buttsons),
-                                   )
+                                   entities=ent)
             elif message.caption:
                 txt = await replace_mdisk_link(message.caption)
                 if message.photo:
                     await message.edit_caption(caption=txt,
                                                reply_markup=InlineKeyboardMarkup(buttsons),
-                                               )
+                                               caption_entities=ent)
                 elif message.document:
                     await message.edit_caption(caption=txt,
                                                reply_markup=InlineKeyboardMarkup(buttsons),
-                                               )
+                                               caption_entities=ent)
         # For text messages
 
         elif message.text:
             text = message.text
             text = await replace_mdisk_link(text)
-            await message.edit_text(text)
+            await message.edit_text(text, entities=ent)
 
         # For media or document messages
 
@@ -67,4 +69,4 @@ async def channel_link_handler(bot, message: Message):
             if link == text:
                 print("The given link is either excluded domain link or a droplink link")
             else:
-                await message.edit_caption(link)
+                await message.edit_caption(link, caption_entities=ent)
