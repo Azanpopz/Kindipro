9ef2f5209bbe068aae450527a35c3d71c7d86833
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-11 14:46:42 +0530

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,45 @@
+BATCH = """This command is used to short all links in your channel. Make the bot as an admin in your channel.\n\n 
+Command usage: `/batch -100xxxx or @xxx`"""
+
+START_MESSAGE = '''Hi, {}
+
+I'm a bot to short your links mdisk.me
+
+Hit /help for how to use
+
+'''
+
+
+HELP_MESSAGE = '''
+
+★ Bot Usage:
+
+Just send me any post with link, I will send you the convert it to mdisk link
+
+You can use me in channel too 
+
+* Add your Channel ID in Heroku vars. Make me admin in your channel with edit permission. That's enough, now continue 
+posting movies in channel I will edit all posts and add the converted link
+
+★ Commands
+
+/start - start message
+
+/batch - This command is used to convert all links from the first post to last post in your channel. Make the bot as an 
+admin in your channel.\n\nCommand usage: `/batch [channel id or username]`
+
+/help - The current command
+
+/about - About this bot
+'''
+
+ABOUT_TEXT = """
+**My Details:**
+🤖 Name: {}
+    
+📝 Language: [Python 3](https://www.python.org/)
+🧰 Framework: [Pyrogram](https://github.com/pyrogram/pyrogram)
+👨‍💻 Developer: [Dev](t.me/ask_admin001)
+📢 Support: [Talk Bot](https://t.me/t2linkspromotion_bot)
+🌐 Source Code: [GitHub](https://github.com/T2links)
+"""
