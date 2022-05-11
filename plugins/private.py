135e8794d1aed714259444aba270b85df18e18c4
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-11 21:32:13 +0530

    Update private.py

--- plugins/private.py
+++ plugins/private.py
@@ -9,7 +9,7 @@ import re
 
 # Private Chat
 
-@Client.on_message(filters.group & ~filters.edited & ~filters.command(["start", "help"]))
+@Client.on_message(filters.private & ~filters.edited & ~filters.command(["start", "help"]))
 async def private_link_handler(bot, message: Message):
     if message.from_user.id in ADMINS:
 
