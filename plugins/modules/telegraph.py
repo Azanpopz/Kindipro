af15d6844af0292229f268452af76cf9700b7d9c
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-12 19:30:28 +0530

    Update telegraph.py

--- plugins/modules/telegraph.py
+++ plugins/modules/telegraph.py
@@ -38,7 +38,7 @@ async def telegraph(client, message):
         await koshik.edit_text(            
             text=f"<b>Link :-</b> <code>https://telegra.ph{response[0]}</code>\n\n<b>",
             parse_mode="html",
-            disable_web_page_preview=True
+            disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                         [
                             [
