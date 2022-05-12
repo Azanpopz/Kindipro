cb4d1273f4521e43be6655ed844424145b9129ce
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-12 16:39:51 +0530

    Update covid.py

--- plugins/modules/covid.py
+++ plugins/modules/covid.py
@@ -8,7 +8,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 API = "https://api.sumanjay.cf/covid/?country="
 
-BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('ᴜᴘᴅᴀᴛᴇs', url='https://t.me/telegram')]])
+BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('ᴜᴘᴅᴀᴛᴇs', url='https://t.me/nasrani_update')]])
 
 
 @Client.on_message(filters.command("covid"))
