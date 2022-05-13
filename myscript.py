eb45aa136b11621032cd8d20bd3c6fd024582839
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-13 20:40:41 +0530

    Update myscript.py

--- myscript.py
+++ myscript.py
@@ -1,33 +1,33 @@
 class script(object):
-    DOWN = "<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
+    DOWN = """<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
 Song /song or /mp3
 Video /video or ytdl
 Lyrics /lyrics """
 
-    CONV = "<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
+    CONV = """<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
 Pdf to image /convpdf
 Link shortner /short
 Telegraph /telegraph
 sticker to image- just send your image
 Attach link /attach"""
    
-    SEARCH = "<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
+    SEARCH = """<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
 Imdb /imdb
 Search /search
 Torrent /torrent
 Image /img """
 
-    INFO = "<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>"""
+    INFO = """<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>"""
    
-    STATS = "<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
+    STATS = """<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
 Heroku stats /botstats
 Bot stats""" /stats
 
-    TRANS = "<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
+    TRANS = """<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
 G Trans /tr [your language code]
 Tts""" /tts
 
-    STICKER = "<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
+    STICKER = """<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
 Sticker pack /kang
 Sticker id /stickerid
 Get sticker /getsticker
@@ -35,24 +35,24 @@ Create sticker /create
 Find sticker /findsticker
 Quote /quote and text without command - /q command with text""" 
 
-    COUNTRY = "<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
+    COUNTRY = """<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
 Covid /covid
 Country /country """
 
-    EXTRA = "<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
+    EXTRA = """<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
 Manuel Filter auto delete
 Auto Filter Auto Delete
 Batch safe group send files
 Movies Request Bot Pm
 Admin contact Bot Pm"""
 
-    ADMINS = "<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
+    ADMINS = """<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
 Broadcast /broadcast
 Mdisk link only privete
 Logs /logs
 Chats /chats """
 
-    USER = "<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
+    USER = """<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
 Info /info
 Id /id
 Json /json
