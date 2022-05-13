4351c66e4d7ee8c7122a40e6bf7f437ddfc6c100
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-13 14:47:35 +0530

    Update test.py

--- plugins/test.py
+++ plugins/test.py
@@ -1,7 +1,7 @@
 # (c) @KoshikKumar17
 import os
 from pyrogram import Client as Koshik
-from pyrogram import filters
+from pyrogram import filters, Message
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
 
 BTN = InlineKeyboardMarkup([[InlineKeyboardButton('✨ 🇮🇳 ❤️ INDIA ❤️ 🇮🇳✨', url='https://india.gov.in')]])
@@ -17,9 +17,9 @@ INDIAN = """▬▬ ▬▬ ▬▬  ▬▬ ▬▬ ▬▬\nSEARCHING...    𝟏𝟎
 
 
 
-@Koshik.on_message(filters.private & filters.command(["india"]))
-async def india_art(bot, update):
-        await update.reply_chat_action("typing")
+@Koshik.on_message(filters.command(["india"]))
+async def india_art(bot, message):
+        await message.reply_chat_action("typing")
         px = await update.reply_text(A,quote=True)
         await px.edit_text(text=B, reply_markup=BTN)
         await px.edit_text(text=C, reply_markup=BTN)
