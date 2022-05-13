8898b6910ff0851659f71db49f1e895e80c123b7
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-13 11:48:16 +0530

    Delete test.py

--- plugins/modules/test.py
+++
@@ -1,7 +0,0 @@
-import random
-lines = open('C:\\Users\\User\\Desktop\\singnplur.txt').read().splitlines()
-myline =random.choice(lines)
-
-@Client.on_message(filters.command('rng') & filters.private)
-def command1(bot, message):
-    bot.send_message(message.chat.id, myline)
