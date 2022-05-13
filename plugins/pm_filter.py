2c30474ed9b88dfb4582c5a6e454be757ecc14f5
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-13 22:54:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1190,7 +1190,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
         await query.message.edit_text(
 
-            text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
+            text=script.MENU_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
 
             reply_markup=reply_markup,
 
