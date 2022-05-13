bd1ec9eae858fc5fcc439022e33277ddbe0ebaf6
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-13 16:28:40 +0530

    Create myscript.py

---
+++ myscript.py
@@ -0,0 +1,66 @@
+class script(object):
+    DOWN = "<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
+Song /song or /mp3
+Video /video or ytdl
+Lyrics /lyrics """
+
+    CONV = "<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
+Pdf to image /convpdf
+Link shortner /short
+Telegraph /telegraph
+sticker to image- just send your image
+Attach link /attach"""
+   
+    SEARCH = "<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
+Imdb /imdb
+Search /search
+Torrent /torrent
+Image /img """
+
+    INFO = "<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>"""
+   
+    STATS = "<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
+Heroku stats /botstats
+Bot stats""" /stats
+
+    TRANS = "<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
+G Trans /tr [your language code]
+Tts""" /tts
+
+    STICKER = "<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
+Sticker pack /kang
+Sticker id /stickerid
+Get sticker /getsticker
+Create sticker /create
+Find sticker /findsticker
+Quote /quote and text without command - /q command with text""" 
+
+    COUNTRY = "<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
+Covid /covid
+Country /country """
+
+    EXTRA = "<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
+Manuel Filter auto delete
+Auto Filter Auto Delete
+Batch safe group send files
+Movies Request Bot Pm
+Admin contact Bot Pm"""
+
+    ADMINS = "<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
+Broadcast /broadcast
+Mdisk link only privete
+Logs /logs
+Chats /chats """
+
+    USER = "<a href='https://telegra.ph/file/eaf97e4782f05b667e551.jpg'>.</a>
+Info /info
+Id /id
+Json /json
+Ban /ban
+Unban /unban
+Connect /connect
+Connections /connection
+Batch /batch
+filter /filter or add
+Paste /paste"""
+                                   
