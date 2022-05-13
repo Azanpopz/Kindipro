bf02ab28df581e847fc1d0b11e5f00944c1a2836
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-13 11:29:25 +0530

    Update test.py

--- plugins/modules/test.py
+++ plugins/modules/test.py
@@ -8,4 +8,6 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyb
 
 @Client.send_poll(filters.command(["poll"]))
 
-await app.send_poll(chat_id, "Is this a poll question?", ["Yes", "No", "Maybe"])
+await app.send_poll(
+chat_id=chat_id
+poll=f"Is this a poll question?", ["Yes", "No", "Maybe"])
