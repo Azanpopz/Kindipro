ec58b574aa2f1f4801837a1ce4b826a65c4933a4
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-14 13:31:54 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -19,7 +19,7 @@ async def save_group(bot, message):
     if temp.ME in r_j_check:
         if not await db.get_chat(message.chat.id):
             total=await bot.get_chat_members_count(message.chat.id)
-            count = await bot.get_chat_members_count(chat_id) 
+            count = await bot.get_chat_members_count(message.chat_id) 
             r_j = message.from_user.mention if message.from_user else "Anonymous" 
             buttons = [[            
             InlineKeyboardButton('🕵️𝐇𝐞𝐥𝐩🕵️', callback_data='help'),
