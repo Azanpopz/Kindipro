799f1d84cda35d36a60b39d7c0fc88415c4ecc50
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-14 21:46:19 +0530

    Update kang.py

--- plugins/modules/Sticker/kang.py
+++ plugins/modules/Sticker/kang.py
@@ -1,10 +1,47 @@
+
+"""
+MIT License
+
+Copyright (c) 2021 TheHamkerCat
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+from io import BytesIO
+from traceback import format_exc
 from io import BytesIO
 from traceback import format_exc
 from pyrogram import Client, filters
 from pyrogram import filters
+from pyrogram import filters
 from pyrogram.types import Message
-from plugins.modules.Sticker.wb import capture_err
 
+from wbb import SUDOERS, USERBOT_PREFIX, app, app2, arq
+from wbb.core.decorators.errors import capture_err
+
+__MODULE__ = "Quotly"
+__HELP__ = """
+/q - To quote a message.
+/q [INTEGER] - To quote more than 1 messages.
+/q r - to quote a message with it's reply
+
+Use .q to quote using userbot
+"""
 
 
 async def quotify(messages: list):
@@ -31,7 +68,8 @@ def isArgInt(message: Message) -> list:
         return [False, 0]
 
 
-@Client.on_message(filters.command("qq") & ~filters.private & ~filters.edited)
+@app2.on_message(filters.command("q", prefixes=USERBOT_PREFIX) & SUDOERS)
+@app.on_message(filters.command("q") & ~filters.private & ~filters.edited)
 @capture_err
 async def quotly_func(client, message: Message):
     if not message.reply_to_message:
