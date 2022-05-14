e8eb24df60ce30f670740336bde153ba56b98fcd
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-14 19:17:37 +0530

    Update wb.py

--- plugins/modules/Sticker/wb.py
+++ plugins/modules/Sticker/wb.py
@@ -10,7 +10,7 @@ from functools import wraps
 
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
-from INFO import AUTH_CHANNEL
+from info import LOG_CHANNEL
 
 
 def split_limits(text):
