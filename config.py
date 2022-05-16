893f911fd4b159dfa1bdd19ce21338c5f421330b
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-16 19:00:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ B = bool(os.environ.get("B"))
 from os import environ
 
 
-class Config(object):
+class Con(object):
     API_ID = int(environ.get("API_ID", 0))
     API_HASH = environ.get("API_HASH", "")
     BOT_TOKEN = environ.get("BOT_TOKEN", "")
