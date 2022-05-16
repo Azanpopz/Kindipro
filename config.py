72e7bd16c1c40becfc94bdc88e7279f651fb5ee9
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-16 18:49:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,4 +34,4 @@ class Config(object):
     API_ID = int(environ.get("API_ID", 0))
     API_HASH = environ.get("API_HASH", "")
     BOT_TOKEN = environ.get("BOT_TOKEN", "")
-    MONGO_STR = environ.get("MONGO_STR", "")
+    DB_URL = environ.get("DB_URL", "")
