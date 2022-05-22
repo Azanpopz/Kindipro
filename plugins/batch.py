e1260c079a7ae7169bf17a125b474e7aa13487b1
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-22 21:46:35 +0530

    Update batch.py

--- plugins/batch.py
+++ plugins/batch.py
@@ -26,7 +26,7 @@ cancel_button = [[
 channel_id = ""
 
 
-@Client.on_message(filters.private & filters.command('batch'))
+@Client.on_message(filters.private & filters.command('mdisk'))
 async def batch(c, m):
     if m.from_user.id in ADMINS:
 
