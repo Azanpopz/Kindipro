41a60c469f9f2ce9065d379f86bd3ea9a7ede1ca
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-07-12 22:34:20 +0530

    Update menu_button.py

--- plugins/modules/menu_button.py
+++ plugins/modules/menu_button.py
@@ -14,7 +14,7 @@ from telegram.ext import CommandHandler
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
-updater.start_polling()
+
 
 def echo(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
