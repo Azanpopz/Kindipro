1bcddb3d3068c27fa93409f98252e4e8aea6ba3f
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-08-27 14:10:15 +0530

    Update auto_approve.py

--- plugins/modules/auto_approve.py
+++ plugins/modules/auto_approve.py
@@ -23,13 +23,13 @@ async def autoapprove(client: pr0fess0r_99, message: ChatJoinRequest):
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
     if APPROVED == "on":
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'https://t.me/nasrani_update')
+            InlineKeyboardButton('🧩𝐉𝐎𝐈𝐍 𝐆𝐑𝐎𝐔𝐏🧩', url=f'https://t.me/nasrani_update')
             
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await client.send_message(chat_id=chat.id, text=TEXT.format(mention=user.mention, title=chat.title),
         reply_markup=reply_markup,
-        parse_mode=enums.ParseMode.HTML
+        parse_mode=parse_mode='html'
     )
         print("Welcome....")
 
