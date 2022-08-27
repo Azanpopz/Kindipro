1f72d94bf2df01e4f1154b7eceb5c344df1df9af
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-08-27 13:45:56 +0530

    Update auto_approve.py

--- plugins/modules/auto_approve.py
+++ plugins/modules/auto_approve.py
@@ -11,7 +11,7 @@ pr0fess0r_99=Client(
     api_hash = os.environ["API_HASH"]
 )
 
-CHAT_ID=int(os.environ.get("-1001775347865", None))
+CHAT_ID=int(os.environ.get("CHAT_ID", None))
 TEXT=os.environ.get("APPROVED_WELCOME_TEXT", "Hello {mention}\nWelcome To {title}\n\nYour Auto Approved")
 APPROVED = os.environ.get("APPROVED_WELCOME", "on").lower()
 
