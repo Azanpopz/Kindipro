f7730cfb810bd779030953d350c56930d048f3d3
devilcomof <112283064+devilcomof@users.noreply.github.com>
2022-09-04 12:10:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -23,7 +23,7 @@ def redirected_env(value):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['API_ID']
+API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
 
