84ffce9a65aea62b61119596944898bd84d9d358
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-02-21 01:48:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Azanpopz/Kindipro.git /Kindipro
+  git clone https://github.com/CyberTG/Kindipro.git /Kindipro
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Kindipro
+  git clone $UPSTREAM_REPO /Kindirpo
 fi
-cd /Kindipro
+cd /master_personal
 pip3 install -U -r requirements.txt
 echo "Starting Kindipro...."
 python3 bot.py
+
