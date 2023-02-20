1e84aabcf74627d81fabc419588086200351e4a0
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-02-21 01:49:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/CyberTG/Kindipro.git /Kindipro
+  git clone https://github.com/Azanpopz/Kindipro.git /Kindipro
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Kindirpo
