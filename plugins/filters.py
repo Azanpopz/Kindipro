975c373297719a09121b70b8940efa07445b1135
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-02-21 19:03:51 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -1,5 +1,5 @@
 import io
-from pyrogram import filters, Client
+from pyrogram import filters, Client, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from database.filters_mdb import(
    add_filter,
