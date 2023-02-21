4aff5a5c0f3768da1c016f90de246cc280f9e65c
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-02-22 02:25:25 +0530

    Update auto_approve.py

--- plugins/modules/auto_approve.py
+++ plugins/modules/auto_approve.py
@@ -1,5 +1,5 @@
 import os
-from pyrogram import Client, filters
+from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, User, ChatJoinRequest
 from myscript import script
 
@@ -14,6 +14,15 @@ pr0fess0r_99=Client(
 CHAT_ID=int(os.environ.get("CHAT_ID", None))
 TEXT=os.environ.get("APPROVED_WELCOME_TEXT", "ʜᴇʟʟᴏ {mention}\nᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴍʏ ᴄʜᴀɴɴᴇʟ.{title}\n\nᴏɴʟʏ ɴᴇᴡ ᴀɴᴅ ʟᴏᴡ ꜱɪᴢᴇ ᴍᴏᴠɪᴇ ᴀᴠᴀɪʟᴀʙʟᴇ. ᴇɴᴊᴏʏɪɴɢ🔥🔥")
 APPROVED = os.environ.get("APPROVED_WELCOME", "on").lower()
+
+@Client.on_message(filters.private & filters.command(["🙂"]))
+async def start(client: pr0fess0r_99, message: Message):
+    approvedbot = await client.get_me() 
+    button = [[ InlineKeyboardButton("📦 Repo", url="t.me/nasrani_update"), InlineKeyboardButton("Updates 📢", url="t.me/nasrani_update") ],
+              [ InlineKeyboardButton("➕️ Add Me To Your Chat ➕️", url=f"http://t.me/{approvedbot.username}?startgroup=botstart") ]]
+    await client.send_message(chat_id=message.chat.id, text=f"**__Hello {message.from_user.mention} Iam Auto Approver Join Request Bot Just [Add Me To Your Group Channnl](http://t.me/{approvedbot.username}?startgroup=botstart) || Repo t.me/nasrani_update||**__", reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview=True)
+
+
 
 @Client.on_chat_join_request(filters.chat(CHAT_ID))
 async def autoapprove(client: pr0fess0r_99, message: ChatJoinRequest):
