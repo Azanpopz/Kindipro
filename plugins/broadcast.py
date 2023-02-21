08dda1ad6ef4abb9fec93e43d3286d36b4e9c5dc
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-02-21 17:36:54 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 import datetime
 import time
 from database.users_chats_db import db
-from info import ADMINS
+from infos import ADMINS
 from utils import broadcast_messages
 import asyncio
         
